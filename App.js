0188a95024c74c197687a43dd207d7bbe1d1570a
yikeanna <yikeanna@gmail.com>
2022-01-15 15:23:37 -0500

    removed comments

--- App.js
+++ App.js
@@ -6,17 +6,13 @@ import FootBar from './components/FootBar'
 export default function App() {
   return (
     <View style={styles.container}>
-
       <View style = {styles.tasksWrapper}>
         <Text style = {styles.sectionTitle}> Today's tasks</Text>
-
       </View>
 
       <View style= {styles.items}>
           <FootBar text={'helllo'}/>
-          {/* <FootBar></FootBar> */}
       </View>
-
     </View>
     
   );
@@ -26,25 +22,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'pink',
-    // position:'relative'
 
   },
   tasksWrapper:{
     paddingTop:80,
     paddingHorizontal:20,
-    // flex:1,
   },
   sectionTitle:{
     fontSize:24,
     fontWeight:'bold'
   },
   items:{
-    // flex: 1,
-    // position:'absolute',
-    // bottom:0,
-    // height: 100
-    // justifyContent: 'flex-end',
-
     position: 'absolute', left: 0, right: 0, bottom: 0
   },
 });
--- components/FootBar.js
+++ components/FootBar.js
@@ -5,18 +5,10 @@ import { Image } from 'react-native'
 const FootBar = (props) => {
     
     return(
-
         <View style={styles.item}>
-            {/* <View style = {styles.itemLeft}> */}
-                
-                {/* <Text style ={styles.itemText}>{props.text}</Text> */}
-                {/* <TouchableOpacity style={styles.square}></TouchableOpacity> */}
-            {/* </View> */}
-            {/* <Circle cx="50" cy="50" r="45" stroke="blue" strokeWidth="2.5" fill="green" /> */}
             <Image style={styles.image} source = {require('../assets/fridge.png')} />
             <Image style={styles.image} source = {require('../assets/add.png')} />
             <Image style={styles.image} source = {require('../assets/reminders.png')} />
-            {/* <View style={styles.circular}></View> */}
         </View>
     )
 }
@@ -25,11 +17,9 @@ const styles = StyleSheet.create({
     item:{
         backgroundColor:'#FFF',
         padding: 15,
-        // borderRadius:10,
         flexDirection:'row',
         alignItems:'center',
         justifyContent:'space-around',
-        // marginBottom:20,
 
     },
     image:{
@@ -37,20 +27,6 @@ const styles = StyleSheet.create({
         aspectRatio: 2.5, 
         resizeMode: 'contain',
     },
-    // square:{
-    //     width:24,
-    //     height:24,
-    //     backgroundColor:'#55BCF6',
-    //     opacity:0.4
-    // },
-    // itemLeft:{
-    //     flexDirection:'row',
-    //     alignItems:'center',
-    //     flexWrap:'wrap'
-    // },
-    // itemText:{},
-    // circular:{},
-
 
 });
 
