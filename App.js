d52de9f3ec4e18d2c0489763a63bc801e3f25da5
yikeanna <yikeanna@gmail.com>
2022-01-15 21:54:09 -0500

    statusBar,, added ScrollView
    fixed the gray lines, the footbar, add scrollview

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { ProgressViewIOSComponent, StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { ProgressViewIOSComponent, StyleSheet, Text, View, ScrollView, Platform,StatusBar } from 'react-native';
 import FootBar from './components/FootBar'
 import FoodContainer from './components/foodContainer';
 import NavBar from './components/NavBar'
@@ -8,17 +8,18 @@ import Fact from './components/quickFacts.js'
 import { SafeAreaView } from 'react-native';
 import { Nunito_400Medium } from '@expo-google-fonts/nunito';
 import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+// import { ScrollView } from 'react-native-gesture-handler';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-
+      
     {/*taskbar*/}
     <View style={styles.taskbar}>
       <NavBar style= {styles.navBar}/>
     </View>
 
-
+    <ScrollView style={styles.scrollView}>
     {/* FAQ */}
     <View style={styles.faqView}>
         <Fact Title={"Did you know?"} Message={"Packaged bread lasts for 5 to 7 days after the printed date in the pantry...and up to 6 months when stored in the freezer!"} /> 
@@ -32,16 +33,22 @@ export default function App() {
             <FoodContainer foodName={'cheese'} foodWeight={'200'} foodExpiry={'3'} foodPurchaseDate={'01/23/24'}/>
         </View>
       </View>
-
-      <View styles={styles.foodContainer}>
-          <FoodContainer foodName={'cheese'} foodWeight={'200'} foodExpiry={'3'} foodPurchaseDate={'01/23/24'}/>
+      <View style={styles.fruitsContainer}>
+        <View styles={styles.foodContainer}>
+            <FoodContainer foodName={'cheese'} foodWeight={'200'} foodExpiry={'3'} foodPurchaseDate={'01/23/24'}/>
+        </View>
+        <View styles={styles.foodContainer}>
+            <FoodContainer foodName={'cheese'} foodWeight={'200'} foodExpiry={'3'} foodPurchaseDate={'01/23/24'}/>
+        </View>
+        <View styles={styles.foodContainer}>
+            <FoodContainer foodName={'cheese'} foodWeight={'200'} foodExpiry={'3'} foodPurchaseDate={'01/23/24'}/>
+        </View>
+        <View styles={styles.foodContainer}>
+            <FoodContainer foodName={'cheese'} foodWeight={'200'} foodExpiry={'3'} foodPurchaseDate={'01/23/24'}/>
+        </View>
       </View>
-      <View styles={styles.foodContainer}>
-          <FoodContainer foodName={'cheese'} foodWeight={'200'} foodExpiry={'3'} foodPurchaseDate={'01/23/24'}/>
-      </View>
-    </View>
 
-
+    </ScrollView>
       <View style= {styles.footBar}>
           <FootBar />
       </View>
@@ -54,6 +61,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#E5E5E5',
     // paddingTop:40
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight :0,
     fontFamily: Nunito_400Medium,
   },
   title: {
@@ -88,10 +96,26 @@ const styles = StyleSheet.create({
     marginBottom: 5
   },
   fridgeView: {
-    height: 400,
+    height:150,
     backgroundColor: "white",
     marginBottom: 5,
-    padding: 10,
+    padding:10,
+  },
+  scrollView:{
+    // paddingBottom:100,
+    marginBottom:70,
+  },
+  blueRectangle:{
+    // position: 'absolute',
+    backgroundColor:'white',
+    left: 0, 
+    right: 0, 
+    bottom: 0
+  },
+  fruitsContainer:{
+    // marginTop:75,
+    paddingTop:10,
+    backgroundColor:"white"
   },
   footBar:{
     position: 'absolute',
