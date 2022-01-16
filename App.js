14d308bee2bfefbfd0fd004b310337d4751e1323
yikeanna <yikeanna@gmail.com>
2022-01-15 19:10:26 -0500

    Navbar Done

--- App.js
+++ App.js
@@ -12,33 +12,33 @@ import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNat
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-    {/*taskbar*/}
-    <View style={styles.taskbar}>
-
-    </View>
-    <NavBar style= {styles.navBar}/>
-
-    {/* reminders */}
-    <View style={styles.remindersView}>
-
-    </View>
-
-    {/* FAQ */}
-    <View style={styles.faqView}>
-        <Fact Title={"Did you know?"} Message={"Food solves hunger"} /> 
-    </View>
-
-    {/* fridge */}
-    <View style={styles.fridgeView}>
-      <Text style={styles.title}>meat and poultry</Text>
-      <View styles={styles.foodContainer}>
-          <FoodContainer foodName={'cheese'} foodWeight={'200'} foodExpiry={'3'} foodPurchaseDate={'01/23/24'}/>
+      {/*taskbar*/}
+      <View style={styles.taskbar}>
+        <NavBar style= {styles.navBar} foodName="home"/>
       </View>
-    </View>
+      
 
-    <View style= {styles.footBar}>
-        <FootBar />
-    </View>
+      {/* reminders */}
+      <View style={styles.remindersView}>
+
+      </View>
+
+      {/* FAQ */}
+      <View style={styles.faqView}>
+          <Fact Title={"Did you know?"} Message={"Food solves hunger"} /> 
+      </View>
+
+      {/* fridge */}
+      <View style={styles.fridgeView}>
+        <Text style={styles.title}>meat and poultry</Text>
+        <View styles={styles.foodContainer}>
+            <FoodContainer foodName={'cheese'} foodWeight={'200'} foodExpiry={'3'} foodPurchaseDate={'01/23/24'}/>
+        </View>
+      </View>
+
+      <View style= {styles.footBar}>
+          <FootBar />
+      </View>
   </SafeAreaView>
   );
 }
@@ -67,7 +67,7 @@ const styles = StyleSheet.create({
   },
   taskbar:{
     backgroundColor: 'white',
-    height: 70,
+    // height: 70,
     marginBottom: 5,
   },
   foodContainer:{
